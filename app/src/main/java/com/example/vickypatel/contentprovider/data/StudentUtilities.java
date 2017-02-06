477c23517b4ee56c5913fcbb2f989d885e6b6b85
vicky patel <vicky.patel@hotmail.com>
2017-02-06 14:25:06 -0500

    student CV added

--- app/src/main/java/com/example/vickypatel/contentprovider/data/StudentUtilities.java
+++ app/src/main/java/com/example/vickypatel/contentprovider/data/StudentUtilities.java
@@ -1,9 +1,22 @@
 package com.example.vickypatel.contentprovider.data;
 
+import android.content.ContentValues;
+
+import com.example.vickypatel.contentprovider.data.StudentContract.StudentEntry;
+import com.example.vickypatel.contentprovider.pojo.Student;
+
 /**
  * Created by Vicky Patel on 2/6/2017.
  */
 
 public class StudentUtilities {
+
+    static ContentValues createStudentValues(Student student) {
+        ContentValues studentValues = new ContentValues();
+        studentValues.put(StudentEntry.COLUMN_NAME, student.getName());
+        studentValues.put(StudentEntry.COLUMN_SURNAME, student.getSurname());
+        studentValues.put(StudentEntry.COLUMN_ZIP_CODE, student.getZipCode());
+        return studentValues;
+    }
 
 }
